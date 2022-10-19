c80791434a3e1fa730a5e4e28c94a105d71f90c7
Chip Kent <chipkent@deephaven.io>
2022-10-19 11:47:37 -0600

    Stashing changes.

--- run_me.py
+++ run_me.py
@@ -5,6 +5,41 @@ print("In run_me.py")
 sys.stdout.flush()
 
 import debugpy
+
+# print(dir(debugpy))
+# print(debugpy.__version__)
+# print(debugpy.__file__)
+# print(debugpy.__path__)
+
+# print("debugpy contents")
+# import glob
+# print(glob.glob(f"{debugpy.__path__[0]}/*"))
+# import os
+# os.system(f"ls {debugpy.__path__[0]}")
+# print("find debugpy")
+# os.system("find / -name \*debugpy\*")
+# os.system(f"echo 'find debugpy.__path__' && find {debugpy.__path__[0]}")
+
+# import debugpy._vendored.pydevd as pydevd
+# import pydevd
+# import debugpy
+# pydevd = debugpy._vendored.import_module("pydevd")
+
+# print("pydevd contents")
+# print(dir(pydevd))
+# print(help(pydevd))
+# print(glob.glob(f"{pydevd.__path__}", recursive=True))
+# # print(type(pydevd.pydevd))
+# print("--")
+# print(glob.glob("/usr/local/lib/python3.10/dist-packages/debugpy/_vendored/**"))
+
+# pydevd.settrace(suspend=False, trace_only_current_thread=False)
+
+# import pydevd
+# # pydevd.settrace(host="", port=10001, suspend=False, trace_only_current_thread=False)
+# pydevd.settrace(port=10001, suspend=True, trace_only_current_thread=False)
+
+
 debugpy.listen(("", 10001))
 debugpy.wait_for_client()
 debugpy.breakpoint()
@@ -39,40 +74,40 @@ print(f"Version info: {sys.version_info}")
 # pydevd.settrace(suspend=False, trace_only_current_thread=False)
 
 
-# from deephaven_server import Server
-# server = Server(port=10000, jvm_args=['-Xmx4g'])
-# server.start()
+from deephaven_server import Server
+server = Server(port=10000, jvm_args=['-Xmx4g'])
+server.start()
 
-# # UGP lock is automatically acquired for each query operation
-# from deephaven import ugp
-# ugp.auto_locking = True
+# UGP lock is automatically acquired for each query operation
+from deephaven import ugp
+ugp.auto_locking = True
 
-# import threading
-# print(f"CALLING: main thread={threading.get_ident()} thread={threading.current_thread()}")
+import threading
+print(f"CALLING: main thread={threading.get_ident()} thread={threading.current_thread()}")
 
-# import mypkg
+import mypkg
 
-# t = mypkg.get_tt()
-# print(t.to_string())
+t = mypkg.get_tt()
+print(t.to_string())
 
-# e = mypkg.get_et()
-# print(e.to_string())
+e = mypkg.get_et()
+print(e.to_string())
 
-# def exec_test():
-#     print(f"CALLING: exec_test thread={threading.get_ident()} thread={threading.current_thread()}")
-#     print("In Exec Test")
+def exec_test():
+    print(f"CALLING: exec_test thread={threading.get_ident()} thread={threading.current_thread()}")
+    print("In Exec Test")
 
-# exec("exec_test()")
+exec("exec_test()")
 
-# def update_func():
-#     print(f"CALLING: update_func thread={threading.get_ident()} thread={threading.current_thread()}")
-#     return 1
+def update_func():
+    print(f"CALLING: update_func thread={threading.get_ident()} thread={threading.current_thread()}")
+    return 1
 
-# t2 = t.update("Y=update_func()")
+t2 = t.update("Y=update_func()")
 
-# while True:
-#     input("Press enter to exit:\n")
-#     break
+while True:
+    input("Press enter to exit:\n")
+    break
 
 print("DONE")
 sys.stdout.flush()
